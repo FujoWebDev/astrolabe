b60b64b636d068dc77708a3ff6f82c6c02ede78b
Essential Randomness <essential.randomn3ss@gmail.com>
2022-07-08 18:41:57 -0700

    Enable selecting and loading image from floating menu

--- src/FloatingMenu.tsx
+++ src/FloatingMenu.tsx
@@ -1,13 +1,59 @@
 import { AddMediaImage } from "iconoir-react";
+import type { Editor } from "@tiptap/react";
 import React from "react";
 
-export const FloatingMenuOptions = (props: {}) => {
+const handleFileLoadRequest = (
+  callback: (loadPromise: Promise<string | ArrayBuffer>) => void
+) => {
+  return (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileInput = e.target;
+    if (fileInput.files != null && fileInput.files[0] != null) {
+      const reader = new FileReader();
+      callback(
+        new Promise((resolve, reject) => {
+          reader.onload = (e) => {
+            if (!e.target?.result) {
+              return;
+            }
+            resolve(e.target.result);
+            fileInput.value = "";
+          };
+          reader.onerror = (e) => {
+            reject();
+          };
+        })
+      );
+      reader.readAsDataURL(fileInput.files[0]);
+    }
+  };
+};
+
+export const FloatingMenuOptions = ({ editor }: { editor: Editor }) => {
   return (
     <ul role="menubar">
       <li role="menuitem">
-        <button aria-label="Add image">
+        <input
+          // TODO: figure out how to namespace this id in case there are multiple editors on the page
+          id="add-image"
+          title="Add an image to the editor"
+          style={{
+            width: 0,
+            height: 0,
+          }}
+          onChange={handleFileLoadRequest(async (fileLoadPromise) => {
+            const loadedPromise = await fileLoadPromise;
+            // TODO: figure out ArrayBuffer type
+            editor.commands.setImage({
+              // @ts-ignore
+              src: loadedPromise,
+            });
+          })}
+          type="file"
+          accept="image/png, image/gif, image/jpeg, image/bmp, image/x-icon"
+        />
+        <label htmlFor="add-image">
           <AddMediaImage />
-        </button>
+        </label>
       </li>
     </ul>
   );
