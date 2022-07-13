0a3674c7537a300dfa089e661516957b93647771
Essential Randomness <essential.randomn3ss@gmail.com>
2022-07-12 20:10:04 -0700

    Use value property for checked in settings menu

--- src/plugins/BlockSettingsMenu/BlockSettingsMenu.tsx
+++ src/plugins/BlockSettingsMenu/BlockSettingsMenu.tsx
@@ -41,7 +41,7 @@ export const ToggleButton = (props: ToggleButtonProps) => {
   return (
     <label>
       <input
-        checked={props.value}
+        value={!!props.value ? "checked" : ""}
         type="checkbox"
         title={props.title}
         onChangeCapture={(e) => props.onValueChange(e.currentTarget.checked, e)}
