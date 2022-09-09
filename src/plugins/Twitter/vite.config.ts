c1be4e927ed793caaa933ea68ac7821959924321
Essential Randomness <essential.randomn3ss@gmail.com>
2022-09-08 19:30:09 -0700

    finish setup of turborepo

---
+++ src/plugins/Twitter/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import linaria from "@linaria/rollup";
+import react from "@vitejs/plugin-react";
+const path = require("path");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "TipTapTwitterNative",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    linaria({
+      sourceMap: process.env.NODE_ENV !== "production",
+    }),
+  ],
+});
