5f94ee78a0e776e899fa8bce7ad7100f9810b81a
Essential Randomness <essential.randomn3ss@gmail.com>
2022-09-11 17:13:57 -0700

    Update tiptap plugin to get it to work with sb7

--- OutputInspectorAddon/src/OutputInspector.tsx
+++ OutputInspectorAddon/src/OutputInspector.tsx
@@ -1,72 +1,31 @@
-import { Extensions, JSONContent, generateHTML } from "@tiptap/core";
+import { useEffect, useState } from "react";
 
+import { CHANNEL_NAME } from ".";
 import CodeMirror from "@uiw/react-codemirror";
-import React from "react";
+import { JSONContent } from "@tiptap/core";
 import ReactJsonView from "react-json-view";
 import { addons } from "@storybook/addons";
 import { dracula } from "@uiw/codemirror-theme-dracula";
 import { html } from "@codemirror/lang-html";
-import htmlParsers from "prettier/parser-html";
-import prettier from "prettier/standalone";
-
-const sanitizeJson = (json: JSONContent) => {
-  return {
-    ...json,
-    content: json.content?.map((item) => {
-      if (!item.attrs || !("src" in item.attrs)) {
-        return item;
-      }
-      return {
-        ...item,
-        attrs: {
-          ...item.attrs,
-          // If we have data:image attributes, these are going to be very long, which makes codemirror not have a good time
-          src: item.attrs.src.substring(0, 1000),
-        },
-      };
-    }),
-  };
-};
-
-const formatHtml = (json: JSONContent, extensions: Extensions) => {
-  const html = generateHTML(json, extensions);
-  return prettier
-    .format(
-      html
-        // linebreaks preceeded by text (that is not a closing tag) should break
-        // on their own line
-        .replaceAll(/[^>]<br( \/)?>/g, "\n<br />\n")
-        // linebreaks followed by text (that is not an opening tag) should move
-        // the text to the following line
-        .replaceAll(/<br( \/)?>[^<]/g, "<br />\n"),
-      {
-        parser: "html",
-        plugins: [htmlParsers],
-      }
-    )
-    .trim();
-};
-
-const CHANNEL_NAME = "CONTENT_UPDATED_CHANNEL";
-export const getContentChangeHandler =
-  (extensions: Extensions) => (json: JSONContent) => {
-    const sanitizedJson = sanitizeJson(json);
-    addons.getChannel().emit(CHANNEL_NAME, {
-      json: sanitizedJson,
-      html: formatHtml(sanitizedJson, extensions),
-    });
-  };
+import { useDebouncedCallback } from "use-debounce";
 
 export const OutputInspector = () => {
-  const [content, setContent] = React.useState<{
+  const [content, setContent] = useState<{
     json: JSONContent;
     html: string;
   }>();
-  React.useEffect(() => {
+  const contentCallback = useDebouncedCallback(
+    (content) => {
+      setContent(content);
+    },
+    1000,
+    { leading: true, trailing: true }
+  );
+  useEffect(() => {
     addons.getChannel().addListener(CHANNEL_NAME, (content) => {
-      React.startTransition(() => {
-        setContent(content);
-      });
+      // TODO: change this with start transition once you can use React 18 in
+      // Storybook addons
+      contentCallback(content);
     });
   }, []);
 
