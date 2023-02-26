37be8ea0d55a2f6d1b3d26a4c6e981d32dbe1ccd
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-02-26 11:22:08 -0800

    fix firefox ws connection

--- storybook/.storybook/main.mjs
+++ storybook/.storybook/main.mjs
@@ -14,6 +14,9 @@ export default {
   features: {
     storyStoreV7: true,
   },
+  docs: {
+    autodocs: "tag",
+  },
   staticDirs: ["../stories/utilities/mocks/"],
 
   async viteFinal(config, { configType }) {
