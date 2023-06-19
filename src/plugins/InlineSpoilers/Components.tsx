c1b65e9be463634262b5a90a12b95f6291860a36
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-06-19 10:01:48 -0700

    Switch to node and make markup work

---
+++ src/plugins/InlineSpoilers/Components.tsx
@@ -0,0 +1,48 @@
+import { InlineSpoilersOptions, PLUGIN_NAME } from "./Plugin";
+import { NodeViewContent, NodeViewProps, NodeViewWrapper } from "@tiptap/react";
+
+import React from "react";
+
+export const EditableInlineSpoilersComponent = (
+  props: Partial<NodeViewProps> & Required<Pick<NodeViewProps, "node">>
+) => {
+  const attributes = props.node.attrs;
+  return (
+    <NodeViewWrapper data-type={PLUGIN_NAME} as="span">
+      <span
+        data-type={PLUGIN_NAME}
+        aria-label="text spoilers"
+        className="inline-spoilers"
+        data-visible={attributes.visible}
+      >
+        <NodeViewContent as="span" />
+      </span>
+    </NodeViewWrapper>
+  );
+};
+
+export const InlineSpoilerComponent = (
+  props: Partial<NodeViewProps> & Required<Pick<NodeViewProps, "node">>
+) => {
+  const attributes = props.node.attrs;
+  return (
+    <NodeViewWrapper data-type={PLUGIN_NAME} as="span">
+      <button
+        data-type={PLUGIN_NAME}
+        aria-label="text spoilers"
+        className="inline-spoilers"
+        data-visible={attributes.visible}
+        onClick={(e) => {
+          const currentVisibility =
+            e.currentTarget.getAttribute("data-visible");
+          e.currentTarget.setAttribute(
+            "data-visible",
+            currentVisibility === "false" ? "true" : "false"
+          );
+        }}
+      >
+        <NodeViewContent as="span" />
+      </button>
+    </NodeViewWrapper>
+  );
+};
