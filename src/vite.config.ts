da6dd4e2da08407c6dc441f2a2dac7f15d896ca3
Essential Randomness <essential.randomn3ss@gmail.com>
2024-02-22 18:11:11 -0800

    change vite config for editor export

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,29 +1,37 @@
 import { defineConfig } from "vite";
 import linaria from "@linaria/vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, "./editor/index.tsx"),
       name: "BobaEditor",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
     },
-    // rollupOptions: {
-    //   external: ["react", "react-dom", "react/jsx-runtime", "react-dom/server"],
-    //   output: {
-    //     globals: {
-    //       react: "React",
-    //       "react-dom": "ReactDOM",
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime", "react-dom/server"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
   plugins: [
     react(),
     linaria({
       sourceMap: process.env.NODE_ENV !== "production",
     }),
+    dts({ tsConfigFilePath: "../tsconfig.json" }),
   ],
+  optimizeDeps: {
+    exclude: ["react", "react-dom"],
+  },
 });
