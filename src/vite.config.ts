5cfab92983f16b6d3e822524cc74f1f207248b6e
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2024-10-26 01:30:19 -0700

    Try to suppress vite use client warnings

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -10,15 +10,28 @@ export default defineConfig({
       entry: path.resolve(__dirname, "./editor/index.tsx"),
       name: "BobaEditor",
     },
-    // rollupOptions: {
-    //   external: ["react", "react-dom"],
-    //   output: {
-    //     globals: {
-    //       react: "React",
-    //       "react-dom": "ReactDOM",
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      // Suppressing this warning from coming up for every iconoir-react file.
+      // It fine to ignore, see https://github.com/TanStack/query/pull/5161#issuecomment-1476840472
+      // It seems like this is fixed automatically in updated versions of Vite,
+      // but I'm not prepared to upgrade Vite right now.
+      onwarn(warning, warn) {
+        if (
+          warning.code === "MODULE_LEVEL_DIRECTIVE" &&
+          warning.message.includes(`"use client"`)
+        ) {
+          return;
+        }
+        warn(warning);
+      },
+      //   external: ["react", "react-dom"],
+      //   output: {
+      //     globals: {
+      //       react: "React",
+      //       "react-dom": "ReactDOM",
+      //     },
+      //   },
+    },
   },
   plugins: [
     react(),
