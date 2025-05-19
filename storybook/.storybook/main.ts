2aaf24073658a28612c5712d8a400ecbaffeaf0b
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-05-19 15:34:13 -0700

    spoiler story

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -8,8 +8,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
   framework: {
     name: "@storybook/react-vite",
