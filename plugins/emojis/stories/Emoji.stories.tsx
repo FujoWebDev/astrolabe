82bae821ba184c5b9b20d47e68b1db3d8bcb672a
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-10-31 14:15:23 -0700

    get second emojiset in

--- plugins/emojis/stories/Emoji.stories.tsx
+++ plugins/emojis/stories/Emoji.stories.tsx
@@ -15,28 +15,40 @@ const meta = {
 			editorTreeViews: [],
 		},
 	},
-	args: {
-		// @ts-expect-error - need to add this to the global args
-		plugins: [
-			EmojiPlugin.configure({
-				sets: ["at://did:plc:r2vpg2iszskbkegoldmqa322/com.fujocoded.astrolabe.emojiset/bobatan-og"]
-			})
-			// Load preferences from com.fujocoded.astrolabe.settings
-			// Might include settings for favorite emoji sets, animated gifs, memes, 
-			// or extra plugins to load if we're ever that fancy
-			// Astrolabe.configure({
-			// 	did: "at://did:plc:r2vpg2iszskbkegoldmqa322"
-			// })
-		],
+   argTypes: {
+	emojiSets: {
+		control: { type: 'check' },
+		options: ['bobatan-og', 'pikashock'],
+		mapping: {
+			'bobatan-og': "at://did:plc:r2vpg2iszskbkegoldmqa322/com.fujocoded.astrolabe.emojiset/bobatan-og",
+			"pikashock": "at://did:plc:dg2qmmjic7mmecrbvpuhtvh6/com.fujocoded.astrolabe.emojiset/pikashock"
+		},
+		default: ['bobatan-og'],
+		
 	},
-	decorators: [withEditorTreeViewer],
+	initialText: {
+		control: false,
+	},
+},
+		decorators: [
+		(Story, context) => {
+			const { args } = context;
+			return withEditorTreeViewer(Story, {
+				...context,
+				args: {
+					...args,
+					plugins: [EmojiPlugin.configure({ sets: args.emojiSets ?? [] })]
+				}
+			});
+		}
+		],
 	component: () => null,
-} satisfies Meta<EditorProviderProps & { initialText: string }>;
+} satisfies Meta<EditorProviderProps & { initialText: string, emojiSets: string[] }>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Editable: Story = {
+export const EditableSingleSet: Story = {
 	args: {
 		initialText:
 			`<p>
@@ -48,7 +60,27 @@ export const Editable: Story = {
 				Want yours? Just type <code>:bobadab:</code>, <code>:bobajustright:</code> or <code>:bobacorn:</code> below 👇 Find <a href="https://pdsls.dev/at://did:plc:r2vpg2iszskbkegoldmqa322/com.fujocoded.astrolabe.emojiset/bobatan-og">the full ATproto record for this emojiset</a> on pdsls.dev!
 			</p>
 			<p>
-			</p>`,
+		</p>`,
+		emojiSets: ['bobatan-og'],
+	},
+};
+
+export const EditableMultipleSets: Story = {
+	args: {
+		// TODO: add an emoji from pikashock too
+		initialText:
+			`<p>
+				<span data-type="emoji" data-name="bobatan-og-bobadab" data-set="at://did:plc:r2vpg2iszskbkegoldmqa322/com.fujocoded.astrolabe.emojiset/bobatan-og" data-fallback="🦝🆒">
+					<img src="https://lionsmane.us-east.host.bsky.network/xrpc/com.atproto.sync.getBlob?did=did%3Aplc%3Ar2vpg2iszskbkegoldmqa322&cid=bafkreifrofbgtrifwocua34nfyerifj27fkzsglhk4vnbsjezwyuifjoqu" alt="bobatan-og-bobadab emoji">
+				</span>
+			</p>
+			<p>
+				Want yours? Just type <code>:bobadab:</code>, <code>:bobajustright:</code> or <code>:bobacorn:</code> below 👇 Find <a href="https://pdsls.dev/at://did:plc:r2vpg2iszskbkegoldmqa322/com.fujocoded.astrolabe.emojiset/bobatan-og">the full ATproto record for this emojiset</a> on pdsls.dev!
+				Want even more? Try <code>:pikagrass:</code>, <code>:pikameh:</code>, <code>:pikastare:</code>, or even <code>:pichu:</code> below 👇 Find <a href="https://pdsls.dev/at://did:plc:dg2qmmjic7mmecrbvpuhtvh6/com.fujocoded.astrolabe.emojiset/pikashock">the full ATproto record for this OTHER emojiset</a> on pdsls.dev!
+			</p>
+			<p>
+		</p>`,
+		emojiSets: ['bobatan-og', 'pikashock'],
 	},
 };
 
@@ -60,5 +92,6 @@ export const ViewOnly: Story = {
 				<img src="https://lionsmane.us-east.host.bsky.network/xrpc/com.atproto.sync.getBlob?did=did%3Aplc%3Ar2vpg2iszskbkegoldmqa322&cid=bafkreicku5fnyj3kuuxqhjzm7ae56ijxz4nxhhdrlgrmtm75afe2k4hxeq" alt="bobatan-og-bobadab emoji">
 			</span>`,
 		editable: false,
+		emojiSets: ['bobatan-og'],
 	},
 };
