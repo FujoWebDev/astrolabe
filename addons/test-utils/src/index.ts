d598d0f828380fc26539fb300256fe651400c969
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-04-28 15:49:19 -0700

    Handle image paste right

--- addons/test-utils/src/index.ts
+++ addons/test-utils/src/index.ts
@@ -1,2 +1,11 @@
 export { BlueskyThread } from "./components/BlueskyThread.js";
 // export { useEditorToRecord } from "./hooks/useEditorToRecord.js";
+
+export function requireElement<T extends Element = HTMLElement>(
+  parent: ParentNode,
+  selector: string,
+): T {
+  const el = parent.querySelector<T>(selector);
+  if (!el) throw new Error(`Expected element matching: ${selector}`);
+  return el;
+}
